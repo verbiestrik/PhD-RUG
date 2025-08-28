ef63e64777530bed45febdffa5e46c72b1e8a68b
Safiere Kuijpers <s.a.kuijpers@rug.nl>
2025-08-28 09:27:12 +0200

    Made plotting more general with options for title, label and colors

--- Spatially Adaptive Moment Models/Python/Nonlinear systems/Visualisation.py
+++ Spatially Adaptive Moment Models/Python/Nonlinear systems/Visualisation.py
@@ -11,9 +11,14 @@ def visualisation(pde_type: PDE.PDE,
                   x2boundary: float,
                   IC: str,
                   data_array: np.array,
+                  title: str,
+                  label: str,
                   **kwargs):
     
     z = np.linspace(0,1,100)
+    color_1 = kwargs["color_1"] if "color_1" in kwargs else 'tab:green'
+    color_2 = kwargs["color_2"] if "color_2" in kwargs else 'greenyellow'
+    color_3 = kwargs["color_3"] if "color_3" in kwargs else 'forestgreen'
 
     if not monte_carlo and not stochastic_galerkin:
         if spatially_adaptive:
@@ -43,53 +48,53 @@ def visualisation(pde_type: PDE.PDE,
         else:
             print("Spatially adaptive and deterministic classical simulations cannot be plotted for orders > 6 at this moment")
         
-        ax1.set_title('Vertical velocity profile at x = {}'.format(x1boundary + (x2boundary - x1boundary)/2))
+        ax1.set_title('Vertical Velocity Profile at x = {0}, x = {1} and x = {2} for {3}'.format(x1boundary + (x2boundary - x1boundary)/4, x1boundary + (x2boundary - x1boundary)/2, x1boundary + 3*(x2boundary - x1boundary)/4, title))
         ax1.plot(velocity_profile[int(resolutionX/2),:], z)
         ax1.set_ylabel('z')
         
-        ax2.set_title('$h$ for order = {0}, IC = {1}'.format(order, IC))
-        ax2.plot(data_array[:,0], data_array[:,1], '-', label='$h$', color='tab:red')
+        ax2.set_title('$h$ for {}'.format(title))
+        ax2.plot(data_array[:,0], data_array[:,1], '-', label='$h$ for {}'.format(label), color=color_1)
         ax2.set_xlabel('x')
         ax2.legend()
         
-        ax3.set_title('$u_m$ for order = {0}, IC = {1}'.format(order, IC))
-        ax3.plot(data_array[:,0], data_array[:,2], '-', label='$u_m$', color='tab:green')
+        ax3.set_title('$u_m$ for {}'.format(title))
+        ax3.plot(data_array[:,0], data_array[:,2], '-', label='$u_m for {}$'.format(label), color=color_1)
         ax3.set_xlabel('x')
         ax3.legend()
 
         if max_order > 0:
-            ax4.set_title(r'$\alpha_1$ for order = {0}, IC = {1}'.format(order, IC))
-            ax4.plot(data_array[:,0], data_array[:,3], '-', label=r'$\alpha_1$', color='tab:orange')
+            ax4.set_title(r'$\alpha_1$ for {}'.format(title))
+            ax4.plot(data_array[:,0], data_array[:,3], '-', label=r'$\alpha_1$ for {}'.format(label), color=color_1)
             ax4.set_xlabel('x')
             ax4.legend()
         
         if max_order > 1:
-            ax5.set_title(r'$\alpha_2$ for order = {0}, IC = {1}'.format(order, IC))
-            ax5.plot(data_array[:,0], data_array[:,4], '-', label=r'$\alpha_2$', color='tab:orange')
+            ax5.set_title(r'$\alpha_2$ for {}'.format(title))
+            ax5.plot(data_array[:,0], data_array[:,4], '-', label=r'$\alpha_2$ for {}'.format(label), color=color_1)
             ax5.set_xlabel('x')
             ax5.legend()
         
         if max_order > 2:
-            ax6.set_title(r'$\alpha_3$ for order = {0}, IC = {1}'.format(order, IC))
-            ax6.plot(data_array[:,0], data_array[:,5], '-', label=r'$\alpha_3$', color='tab:orange')
+            ax6.set_title(r'$\alpha_3$ for {}'.format(title))
+            ax6.plot(data_array[:,0], data_array[:,5], '-', label=r'$\alpha_3$ for {}'.format(label), color=color_1)
             ax6.set_xlabel('x')
             ax6.legend()
         
         if max_order > 3:
-            ax6.set_title(r'$\alpha_4$ for order = {0}, IC = {1}'.format(order, IC))
-            ax6.plot(data_array[:,0], data_array[:,6], '-', label=r'$\alpha_4$', color='tab:orange')
+            ax6.set_title(r'$\alpha_4$ for {}'.format(title))
+            ax6.plot(data_array[:,0], data_array[:,6], '-', label=r'$\alpha_4$ for {}'.format(label), color=color_1)
             ax6.set_xlabel('x')
             ax6.legend()
 
         if max_order > 4:
-            ax6.set_title(r'$\alpha_5$ for order = {0}, IC = {1}'.format(order, IC))
-            ax6.plot(data_array[:,0], data_array[:,7], '-', label=r'$\alpha_5$', color='tab:orange')
+            ax6.set_title(r'$\alpha_5$ for {}'.format(title))
+            ax6.plot(data_array[:,0], data_array[:,7], '-', label=r'$\alpha_5$ for {}'.format(label), color=color_1)
             ax6.set_xlabel('x')
             ax6.legend()
 
         if max_order > 5:
-            ax6.set_title(r'$\alpha_6$ for order = {0}, IC = {1}'.format(order, IC))
-            ax6.plot(data_array[:,0], data_array[:,8], '-', label=r'$\alpha_6$', color='tab:orange')
+            ax6.set_title(r'$\alpha_6$ for {}'.format(title))
+            ax6.plot(data_array[:,0], data_array[:,8], '-', label=r'$\alpha_6$ for {}'.format(label), color=color_1)
             ax6.set_xlabel('x')
             ax6.legend()
     
@@ -120,12 +125,11 @@ def visualisation(pde_type: PDE.PDE,
                 for i in range(len(um_exp)):
                     velocity_profile_exp[i,:] = um_exp[i]*(np.ones(len(z))) + alpha1_exp[i]*(np.ones(len(z)) - 2*z)
                     velocity_profile_var[i,:] = um_var[i]*(np.ones(len(z))) + alpha1_var[i]*(np.ones(len(z)) - 2*z)**2
-                
                 fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2,2)
                 
-                ax4.set_title(r'$\alpha_1$ for order = {0}, IC = {1}'.format(order, IC))
-                ax4.plot(data_array[0,:,0], alpha1_exp, '-', label=r'$\alpha_1$', color='tab:orange')
-                ax4.fill_between(data_array[0,:,0], alpha1_exp - np.sqrt(alpha1_var), alpha1_exp + np.sqrt(alpha1_var),label='$1\sigma$', color='tab:orange', alpha=0.2)
+                ax4.set_title(r'$\alpha_1$ for {}'.format(title))
+                ax4.plot(data_array[0,:,0], alpha1_exp, '-', label=r'$\alpha_1$ for {}'.format(label), color=color_1)
+                ax4.fill_between(data_array[0,:,0], alpha1_exp - np.sqrt(alpha1_var), alpha1_exp + np.sqrt(alpha1_var),label='$1\sigma$ for {}'.format(label), color=color_1, alpha=0.2)
                 ax4.set_xlabel('x')
                 ax4.legend()
             
@@ -133,8 +137,8 @@ def visualisation(pde_type: PDE.PDE,
                 print("This order is not implemented yet for the Monte Carlo loop")
             
             
-            ax2.set_title('$h$ for order = {0}, IC = {1}'.format(order, IC))
-            ax3.set_title('$u_m$ for order = {0}, IC = {1}'.format(order, IC))
+            ax2.set_title('$h$ for {}'.format(title))
+            ax3.set_title('$u_m$ for {}'.format(title))
             x = data_array[0,:,0]
         
         else:
@@ -148,32 +152,36 @@ def visualisation(pde_type: PDE.PDE,
             
             elif mom_order == 1:
                 fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2,2)
-                ax4.set_title(r'$\alpha_1$ for MO = {0}, SO = {1}, IC = {2}'.format(mom_order, SG_order, IC))
-                ax4.plot(data_array[:,0], alpha1_exp, '-', label=r'$\alpha_1$', color='tab:orange')
-                ax4.fill_between(data_array[:,0], alpha1_exp - np.sqrt(alpha1_var), alpha1_exp + np.sqrt(alpha1_var),label='$1\sigma$', color='tab:orange', alpha=0.2)
+                ax4.set_title(r'$\alpha_1$ for {}'.format(title))
+                ax4.plot(data_array[:,0], alpha1_exp, '-', label=r'$\alpha_1$ for {}'.format(label), color=color_1)
+                ax4.fill_between(data_array[:,0], alpha1_exp - np.sqrt(alpha1_var), alpha1_exp + np.sqrt(alpha1_var),label='$1\sigma$ for {}'.format(label), color=color_1, alpha=0.2)
                 ax4.set_xlabel('x')
                 ax4.legend()
             
             else:
                 print("This moment order is not implemented yet for the SGSWME1D")
             
-            ax2.set_title('$h$ for MO = {0}, SO = {1}, IC = {2}'.format(mom_order, SG_order, IC))
-            ax3.set_title('$u_m$ for MO = {0}, SO = {1}, IC = {2}'.format(mom_order, SG_order, IC))
+            ax2.set_title('$h$ for {}'.format(title))
+            ax3.set_title('$u_m$ for {}'.format(title))
             x = data_array[:,0]
                 
-        ax1.set_title('Vertical velocity profile at x = {}'.format(x1boundary + (x2boundary - x1boundary)/2))
-        ax1.plot(velocity_profile_exp[int(resolutionX/2),:], z)
-        ax1.fill_betweenx(z, velocity_profile_exp[int(resolutionX/2),:] - np.sqrt(velocity_profile_var[int(resolutionX/2),:]), velocity_profile_exp[int(resolutionX/2),:] + np.sqrt(velocity_profile_var[int(resolutionX/2),:]), label='$1\sigma$', color='tab:blue', alpha=0.2)
+        ax1.set_title('Vertical Velocity Profile at x = {0}, x = {1} and x = {2} for {3}'.format(x1boundary + (x2boundary - x1boundary)/4, x1boundary + (x2boundary - x1boundary)/2, x1boundary + 3*(x2boundary - x1boundary)/4, title))
+        ax1.plot(velocity_profile_exp[int(resolutionX/4),:], z, label='$u$ at x=-1 for {}'.format(label), color=color_2)
+        ax1.fill_betweenx(z, velocity_profile_exp[int(resolutionX/4),:] - np.sqrt(velocity_profile_var[int(resolutionX/4),:]), velocity_profile_exp[int(resolutionX/4),:] + np.sqrt(velocity_profile_var[int(resolutionX/4),:]), label='$1\sigma$ at x=-1 for {}'.format(label), color=color_2, alpha=0.2)
+        ax1.plot(velocity_profile_exp[int(resolutionX/2),:], z, label='$u$ at x=0 for {}'.format(label), color=color_1)
+        ax1.fill_betweenx(z, velocity_profile_exp[int(resolutionX/2),:] - np.sqrt(velocity_profile_var[int(resolutionX/2),:]), velocity_profile_exp[int(resolutionX/2),:] + np.sqrt(velocity_profile_var[int(resolutionX/2),:]), label='$1\sigma$ at x=0 for {}'.format(label), color=color_1, alpha=0.2)
+        ax1.plot(velocity_profile_exp[int(3*resolutionX/4),:], z, label='$u$ at x=1 for {}'.format(label), color=color_3)
+        ax1.fill_betweenx(z, velocity_profile_exp[int(3*resolutionX/4),:] - np.sqrt(velocity_profile_var[int(3*resolutionX/4),:]), velocity_profile_exp[int(3*resolutionX/4),:] + np.sqrt(velocity_profile_var[int(3*resolutionX/4),:]), label='$1\sigma$ at x=1 for {}'.format(label), color=color_3, alpha=0.2)
         ax1.set_ylabel('z')
         ax1.legend()
             
-        ax2.plot(x, h_exp, '-', label='$h$', color='tab:red')
-        ax2.fill_between(x, h_exp - np.sqrt(h_var), h_exp + np.sqrt(h_var), label='$1\sigma$', color='tab:red', alpha=0.2)
+        ax2.plot(x, h_exp, '-', label='$h$ for {}'.format(label), color=color_1)
+        ax2.fill_between(x, h_exp - np.sqrt(h_var), h_exp + np.sqrt(h_var), label='$1\sigma$ for {}'.format(label), color=color_1, alpha=0.2)
         ax2.set_xlabel('x')
         ax2.legend()
             
-        ax3.plot(x, um_exp, '-', label='$u_m$', color='tab:green')
-        ax3.fill_between(x, um_exp - np.sqrt(um_var), um_exp + np.sqrt(um_var), label='$1\sigma$', color='tab:green', alpha=0.2)
+        ax3.plot(x, um_exp, '-', label='$u_m$ for {}'.format(label), color=color_1)
+        ax3.fill_between(x, um_exp - np.sqrt(um_var), um_exp + np.sqrt(um_var), label='$1\sigma$ for {}'.format(label), color=color_1, alpha=0.2)
         ax3.set_xlabel('x')
         ax3.legend()
 
@@ -181,4 +189,139 @@ def visualisation(pde_type: PDE.PDE,
     else:
         print("Spatial adaptivity is not available for a monte carlo loop or stochastic Galerkin yet")
     
-    plt.show()
+    try:
+        return [ax1, ax2, ax3, ax4, ax5, ax6]
+    except NameError:
+        try:
+            return [ax1, ax2, ax3, ax4, ax5]
+        except NameError:
+            try:
+                return [ax1, ax2, ax3, ax4]
+            except NameError:
+                return [ax1, ax2, ax3]
+
+
+def visualisation_add(ax_arr,
+                      pde_type: PDE.PDE,
+                      spatially_adaptive: bool,
+                      monte_carlo: bool,
+                      stochastic_galerkin: bool,
+                      resolutionX: int,
+                      x1boundary: float,
+                      x2boundary: float,
+                      IC: str,
+                      data_array: np.array,
+                      label: str,
+                      **kwargs):
+    z = np.linspace(0,1,100)
+    color_1 = kwargs["color_1"] if "color_1" in kwargs else 'tab:red'
+    color_2 = kwargs["color_2"] if "color_2" in kwargs else 'lightcoral'
+    color_3 = kwargs["color_3"] if "color_3" in kwargs else 'firebrick'
+
+    if not monte_carlo and not stochastic_galerkin:
+        if spatially_adaptive:
+            order = kwargs["orders"] if "orders" in kwargs else print("Orders should be specified for spatially adaptive plotting")
+            max_order = np.max([int(order_i) for order_i in order.split(',')])
+            velocity_profile = pde_type.compute_vertical_velocity_profile(max_order, data_array, z)
+        
+        else:
+            order = kwargs["order"] if "order" in kwargs else print("Order should be specified for classical plotting")
+            max_order = order
+            velocity_profile = pde_type.compute_vertical_velocity_profile(order, data_array, z)
+        
+        
+        ax_arr[0].plot(velocity_profile[int(resolutionX/2),:], z)
+        ax_arr[1].plot(data_array[:,0], data_array[:,1], '-', label='$h$ for {}'.format(label), color=color_1)
+        ax_arr[2].plot(data_array[:,0], data_array[:,2], '-', label='$u_m$ for {}'.format(label), color=color_1)
+
+        if max_order > 0:
+            ax_arr[3].plot(data_array[:,0], data_array[:,3], '-', label=r'$\alpha_1$ for {}'.format(label), color=color_1)
+        
+        if max_order > 1:
+            ax_arr[4].plot(data_array[:,0], data_array[:,4], '-', label=r'$\alpha_2$ for {}'.format(label), color=color_1)
+        
+        if max_order > 2:
+            ax_arr[5].plot(data_array[:,0], data_array[:,5], '-', label=r'$\alpha_3$ for {}'.format(label), color=color_1)
+        
+        if max_order > 3:
+            ax_arr[6].plot(data_array[:,0], data_array[:,6], '-', label=r'$\alpha_4$ for {}'.format(label), color=color_1)
+
+        if max_order > 4:
+            ax_arr[7].plot(data_array[:,0], data_array[:,7], '-', label=r'$\alpha_5$ for {}'.format(label), color=color_1)
+
+        if max_order > 5:
+            ax_arr[8].plot(data_array[:,0], data_array[:,8], '-', label=r'$\alpha_6$ for {}'.format(label), color=color_1)
+    
+    elif monte_carlo or stochastic_galerkin and not spatially_adaptive:
+        if monte_carlo and stochastic_galerkin:
+            print("Stochastic Galerkin and a monte carlo loop cannot (and should not) be used at the same time")
+        
+        elif monte_carlo:
+            n_MC = kwargs["n_MC"] if "n_MC" in kwargs else print("Number of Monte Carlo samples should be given for Monte Carlo plotting")
+            order = kwargs["order"] if "order" in kwargs else print("Order should be specified for monte carlo loop plotting")
+            h_exp = np.average([data_array[n,:,1] for n in range(n_MC)], axis=0)
+            h_var = np.var([data_array[n,:,1] for n in range(n_MC)], axis=0)
+            um_exp = np.average([data_array[n,:,2] for n in range(n_MC)], axis=0)
+            um_var = np.var([data_array[n,:,2] for n in range(n_MC)], axis=0)
+        
+            velocity_profile_exp = np.zeros((len(um_exp), len(z)))
+            velocity_profile_var = np.zeros((len(um_exp), len(z)))
+            
+            if order == 0:
+                for i in range(len(um_exp)):
+                    velocity_profile_exp[i,:] = um_exp[i]*(np.ones(len(z)))
+                    velocity_profile_var[i,:] = um_var[i]*(np.ones(len(z)))
+
+            elif order == 1:
+                alpha1_exp = np.average([data_array[n,:,3] for n in range(n_MC)], axis=0)
+                alpha1_var = np.var([data_array[n,:,3] for n in range(n_MC)], axis=0)
+                for i in range(len(um_exp)):
+                    velocity_profile_exp[i,:] = um_exp[i]*(np.ones(len(z))) + alpha1_exp[i]*(np.ones(len(z)) - 2*z)
+                    velocity_profile_var[i,:] = um_var[i]*(np.ones(len(z))) + alpha1_var[i]*(np.ones(len(z)) - 2*z)**2
+                
+                ax_arr[3].plot(data_array[0,:,0], alpha1_exp, '-', label=r'$\alpha_1$ for {}'.format(label), color=color_1)
+                ax_arr[3].fill_between(data_array[0,:,0], alpha1_exp - np.sqrt(alpha1_var), alpha1_exp + np.sqrt(alpha1_var),label='$1\sigma$ for {}'.format(label), color=color_1, alpha=0.2)
+                ax_arr[3].legend()
+            
+            else:
+                print("This order is not implemented yet for the Monte Carlo loop")
+            
+            x = data_array[0,:,0]
+        
+        else:
+            mom_order = kwargs["mom_order"] if "mom_order" in kwargs else print("Moment order should be specified for stochastic Galerkin projection plotting")
+            SG_order  = kwargs["SG_order"]  if "SG_order"  in kwargs else print("Stochastic Galerkin order should be specified for stochastic Galerkin projection plotting")
+            velocity_profile_exp, velocity_profile_var = pde_type.compute_vertical_velocity_profile(mom_order, SG_order, data_array[:,1:], z)
+            h_exp, h_var, um_exp, um_var, alpha1_exp, alpha1_var = pde_type.compute_exp_and_var(mom_order, SG_order, data_array[:,1:], True)
+            
+            if mom_order == 1:
+                ax_arr[3].plot(data_array[:,0], alpha1_exp, '-', label=r'$\alpha_1$ for {}'.format(label), color=color_1)
+                ax_arr[3].fill_between(data_array[:,0], alpha1_exp - np.sqrt(alpha1_var), alpha1_exp + np.sqrt(alpha1_var),label='$1\sigma$ for {}'.format(label), color=color_1, alpha=0.2)
+                ax_arr[3].legend()
+
+            else:
+                print("This moment order is not implemented yet for the SGSWME1D")
+            
+            x = data_array[:,0]
+                
+        ax_arr[0].plot(velocity_profile_exp[int(resolutionX/4),:], z, '-', label='$u$ at x=-1 for {}'.format(label), color=color_2)
+        ax_arr[0].fill_betweenx(z, velocity_profile_exp[int(resolutionX/4),:] - np.sqrt(velocity_profile_var[int(resolutionX/4),:]), velocity_profile_exp[int(resolutionX/4),:] + np.sqrt(velocity_profile_var[int(resolutionX/4),:]), label='$1\sigma$ at x=-1 for {}'.format(label), color=color_2, alpha=0.2)
+        ax_arr[0].plot(velocity_profile_exp[int(resolutionX/2),:], z, '-', label='$u$ at x=0 for {}'.format(label), color=color_1)
+        ax_arr[0].fill_betweenx(z, velocity_profile_exp[int(resolutionX/2),:] - np.sqrt(velocity_profile_var[int(resolutionX/2),:]), velocity_profile_exp[int(resolutionX/2),:] + np.sqrt(velocity_profile_var[int(resolutionX/2),:]), label='$1\sigma$ at x=0 for {}'.format(label), color=color_1, alpha=0.2)
+        ax_arr[0].plot(velocity_profile_exp[int(3*resolutionX/4),:], z, '-', label='$u$ at x=1 for {}'.format(label), color=color_3)
+        ax_arr[0].fill_betweenx(z, velocity_profile_exp[int(3*resolutionX/4),:] - np.sqrt(velocity_profile_var[int(3*resolutionX/4),:]), velocity_profile_exp[int(3*resolutionX/4),:] + np.sqrt(velocity_profile_var[int(3*resolutionX/4),:]), label='$1\sigma$ at x=1 for {}'.format(label), color=color_3, alpha=0.2)
+        ax_arr[0].legend()
+ 
+        ax_arr[1].plot(x, h_exp, '-', label='$h$ for {}'.format(label), color=color_1)
+        ax_arr[1].fill_between(x, h_exp - np.sqrt(h_var), h_exp + np.sqrt(h_var), label='$1\sigma$ for {}'.format(label), color=color_1, alpha=0.2)
+        ax_arr[1].legend()
+            
+        ax_arr[2].plot(x, um_exp, '-', label='$u_m$ for {}'.format(label), color=color_1)
+        ax_arr[2].fill_between(x, um_exp - np.sqrt(um_var), um_exp + np.sqrt(um_var), label='$1\sigma$ for {}'.format(label), color=color_1, alpha=0.2)
+        ax_arr[2].legend()
+
+    
+    else:
+        print("Spatial adaptivity is not available for a monte carlo loop or stochastic Galerkin yet")
+    
+    return ax_arr
