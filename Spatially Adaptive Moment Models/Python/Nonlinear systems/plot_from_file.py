a77ec1e8801ea300f356ee4b522443f7e1911fc5
Safiere Kuijpers <s.a.kuijpers@rug.nl>
2025-10-17 16:54:31 +0200

    updated plot from file to current status of main

--- Spatially Adaptive Moment Models/Python/Nonlinear systems/plot_from_file.py
+++ Spatially Adaptive Moment Models/Python/Nonlinear systems/plot_from_file.py
@@ -6,8 +6,8 @@ import matplotlib.pyplot as plt
 IC_A = 'lowDamBreak_withVelocity'
 IC_B = 'lowDamBreak_withVelocity'
 IC_C = 'lowDamBreak_withVelocity'
-pde_type_A = 'SWME1D'
-pde_type_B = 'SGSWME1D'
+pde_type_A = 'SGSWME1D'
+pde_type_B = 'SWME1D'
 pde_type_C = 'SGSWME1D'
 
 spatially_adaptive_A = False
@@ -22,8 +22,10 @@ viscosity_C = 0.1
 slip_length_A = 0.1
 slip_length_B = 0.1
 slip_length_C = 0.1
-linear_source = True
 
+linear_source_implicit_A = True
+linear_source_implicit_B = True
+linear_source_implicit_C = True
 monte_carlo_A = False
 monte_carlo_B = False
 monte_carlo_C = False
@@ -43,13 +45,13 @@ sigma_A = 0.01
 sigma_B = 0.01
 sigma_C = 0.01
 
-stochastic_Galerkin_A = False
+stochastic_Galerkin_A = True
 stochastic_Galerkin_B = True
 stochastic_Galerkin_C = True
 mom_order_A = 1
 mom_order_B = 1
 mom_order_C = 1
-SG_order_A = 1
+SG_order_A = 0
 SG_order_B = 1
 SG_order_C = 2
 
@@ -70,45 +72,51 @@ color_2_C = 'lightskyblue'
 color_3_C = 'mediumblue'
 
 if pde_type_A == 'SWME1D':
-    _pde_A = PDE.SWME1D(IC_A, viscosity_A, slip_length_A, hyperbolic=False, linear_source=True)
+    _pde_A = PDE.SWME1D(IC_A, viscosity_A, slip_length_A, hyperbolic=False, linear_source=linear_source_implicit_A)
 elif pde_type_A == 'HSWME1D':
-    _pde_A = PDE.SWME1D(IC_A, viscosity_A, slip_length_A, hyperbolic=True)
+    _pde_A = PDE.SWME1D(IC_A, viscosity_A, slip_length_A, hyperbolic=True, linear_source=linear_source_implicit_A)
 elif pde_type_A == 'VegetationSWME1D':
-    _pde_A = PDE.VegetationSWME1D(IC_A, viscosity_A, slip_length_A, False, 1, 1, 1)
+    _pde_A = PDE.VegetationSWME1D(IC_A, viscosity_A, slip_length_A, False, linear_source_implicit_A, 0.008, 1, 264)
 elif pde_type_A == 'SGSWME1D' and stochastic_Galerkin_A and not spatially_adaptive_A and not monte_carlo_A:
     _pde_A = PDE.SGSWME1D(IC_A, distr_A, mu_A, sigma_A, slip_length_A, hyperbolic=False)
-elif pde_type_A == 'SGSWME1D':
+elif pde_type_A == 'HSGSWME1D' and stochastic_Galerkin_A and not spatially_adaptive_A and not monte_carlo_A:
+    _pde_A = PDE.SGSWME1D(IC_A, distr_A, mu_A, sigma_A, slip_length_A, hyperbolic=True)
+elif pde_type_A == 'SGSWME1D' or pde_type_A == 'HSGSWME1D':
     print("pde_type can only be SGSWME1D if stochastic_Galerkin is True and spatially_adaptive and monte_carlo are False")
 else:
     print('This pde_type is not implemented yet')
 
-'''
+
 if pde_type_B == 'SWME1D':
-    _pde_B = PDE.SWME1D(IC_B, viscosity_B, slip_length_B, hyperbolic=False)
+    _pde_B = PDE.SWME1D(IC_B, viscosity_B, slip_length_B, hyperbolic=False, linear_source=linear_source_implicit_B)
 elif pde_type_B == 'HSWME1D':
-    _pde_B = PDE.SWME1D(IC_B, viscosity_B, slip_length_B, hyperbolic=True)
+    _pde_B = PDE.SWME1D(IC_B, viscosity_B, slip_length_B, hyperbolic=True, linear_source=linear_source_implicit_B)
 elif pde_type_B == 'VegetationSWME1D':
-    _pde_B = PDE.VegetationSWME1D(IC_B, viscosity_B, slip_length_B, False, 1, 1, 1)
+    _pde_B = PDE.VegetationSWME1D(IC_B, viscosity_B, slip_length_B, False, linear_source_implicit_B, 0.008, 1, 264)
 elif pde_type_B == 'SGSWME1D' and stochastic_Galerkin_B and not spatially_adaptive_B and not monte_carlo_B:
     _pde_B = PDE.SGSWME1D(IC_B, distr_B, mu_B, sigma_B, slip_length_B, hyperbolic=False)
-elif pde_type_B == 'SGSWME1D':
+elif pde_type_B == 'HSGSWME1D' and stochastic_Galerkin_B and not spatially_adaptive_B and not monte_carlo_B:
+    _pde_B = PDE.SGSWME1D(IC_B, distr_B, mu_B, sigma_B, slip_length_B, hyperbolic=True)
+elif pde_type_B == 'SGSWME1D' or pde_type_B == 'HSGSWME1D':
     print("pde_type can only be SGSWME1D if stochastic_Galerkin is True and spatially_adaptive and monte_carlo are False")
 else:
     print('This pde_type is not implemented yet')
 
 if pde_type_C == 'SWME1D':
-    _pde_C = PDE.SWME1D(IC_C, viscosity_C, slip_length_C, hyperbolic=False)
+    _pde_C = PDE.SWME1D(IC_C, viscosity_C, slip_length_C, hyperbolic=False, linear_source=linear_source_implicit_C)
 elif pde_type_C == 'HSWME1D':
-    _pde_C = PDE.SWME1D(IC_C, viscosity_C, slip_length_C, hyperbolic=True)
+    _pde_C = PDE.SWME1D(IC_C, viscosity_C, slip_length_C, hyperbolic=True, linear_source=linear_source_implicit_C)
 elif pde_type_C == 'VegetationSWME1D':
-    _pde_C = PDE.VegetationSWME1D(IC_C, viscosity_C, slip_length_C, False, 1, 1, 1)
+    _pde_C = PDE.VegetationSWME1D(IC_C, viscosity_C, slip_length_C, False, linear_source_implicit_C, 0.008, 1, 264)
 elif pde_type_C == 'SGSWME1D' and stochastic_Galerkin_C and not spatially_adaptive_C and not monte_carlo_C:
     _pde_C = PDE.SGSWME1D(IC_C, distr_C, mu_C, sigma_C, slip_length_C, hyperbolic=False)
-elif pde_type_C == 'SGSWME1D':
+elif pde_type_C == 'HSGSWME1D' and stochastic_Galerkin_C and not spatially_adaptive_C and not monte_carlo_C:
+    _pde_C = PDE.SGSWME1D(IC_C, distr_C, mu_C, sigma_C, slip_length_C, hyperbolic=True)
+elif pde_type_C == 'SGSWME1D' or pde_type_C == 'HSGSWME1D':
     print("pde_type can only be SGSWME1D if stochastic_Galerkin is True and spatially_adaptive and monte_carlo are False")
 else:
     print('This pde_type is not implemented yet')
-'''
+
 
 if spatially_adaptive_A:
     data_array = np.load("Data\data_{0}_orders={1}_nu={2}_lambda={3}_IC={4}_T={5}.npy".format(pde_type_A, orders_A, viscosity_A, slip_length_A, IC_A, t_end))
@@ -127,9 +135,9 @@ elif monte_carlo_A:
 
 else:
     data_array = np.load("Data\data_{0}_order={1}_nu={2}_lambda={3}_IC={4}_T={5}.npy".format(pde_type_A, order_A, viscosity_A, slip_length_A, IC_A, t_end))
-    Visualisation.visualisation(_pde_A, False, False, False, len(data_array[:,0]), data_array[0,0], data_array[-1,0], IC_A, data_array, title, label_A, order = order_A, color_2 = color_2_A, color_3 = color_3_A)
+    ax_arr = Visualisation.visualisation(_pde_A, False, False, False, len(data_array[:,0]), data_array[0,0], data_array[-1,0], IC_A, data_array, title, label_A, order = order_A, color_2 = color_2_A, color_3 = color_3_A)
 
-'''
+
 if spatially_adaptive_B:
     data_array = np.load("Data\data_{0}_orders={1}_nu={2}_lambda={3}_IC={4}_T={5}.npy".format(pde_type_B, orders_B, viscosity_B, slip_length_B, IC_B, t_end))
     ax_arr = Visualisation.visualisation_add(ax_arr,_pde_B, True, False, False, len(data_array[:,0]), data_array[0,0], data_array[-1,0], IC_B, data_array, label_B, orders = orders_B, color_1 = color_1_B, color_2 = color_2_B, color_3 = color_3_B)
@@ -168,5 +176,5 @@ elif monte_carlo_B:
 else:
     data_array = np.load("Data\data_{0}_order={1}_nu={2}_lambda={3}_IC={4}_T={5}.npy".format(pde_type_C, order_C, viscosity_C, slip_length_C, IC_C, t_end))
     ax_arr = Visualisation.visualisation(ax_arr, _pde_C, False, False, False, len(data_array[:,0]), data_array[0,0], data_array[-1,0], IC_C, data_array, label_C, order = order_C, color_1 = color_1_C, color_2 = color_2_C, color_3 = color_3_C)
-'''
+
 plt.show()
